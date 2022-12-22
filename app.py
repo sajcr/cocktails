1db4adf3b49cb376a890914f8eda3095db864925
sajcr <97626066+sajcr@users.noreply.github.com>
2022-12-22 20:05:58 +0000

    check point before adding classes

--- app.py
+++ app.py
@@ -8,44 +8,103 @@ app = Flask(__name__)
 db = SQL("sqlite:///cocktails.db")
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-  
-  cocktails = db.execute("SELECT idDrink, strDrink FROM cocktails")
-  
 
-  
-  if request.args.get("idDrink"):
-    idDrink = request.args.get("idDrink")
-    
-    drink = db.execute("SELECT * FROM cocktails WHERE idDrink = ?", idDrink)[0]
-   
-    ingredients = []
-     
-    for n in range(drink['numIngredients']):
+    #build a list of items we have in cabinet
+    things = db.execute("SELECT ingredient FROM ingredients WHERE have = 1")
+
+    items = []    
+    for thing in things:
+      items.append(thing['ingredient']) 
       
-      item = {
-        "ingredient":drink[f"strIngredient{n+1}"],
-        "measure":drink[f"strMeasure{n+1}"]
-      }
+    print(items)
+
+    #get cocktails details
+    entries = db.execute(
+        "SELECT idDrink, strDrink, strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5, strIngredient6, strIngredient7, strIngredient8, strIngredient9, strIngredient10, strIngredient11, strIngredient12, strIngredient13, strIngredient14, strIngredient15 FROM cocktails"
+    )
     
-      ingredients.append(item)
+    cocktails = []
     
+    for entry in entries:
+      add = True
+      for n in range(15):
+        
+        if entry[f"strIngredient{n+1}"] in items:
+          pass
+        elif entry[f"strIngredient{n+1}"] == "None":
+          pass
+        else:
+          add = False
+      
+      if add == True:
+        cocktails.append(entry)
     
+    print(cocktails)
     
-    return render_template("index.html", cocktails=cocktails, drink=drink, ingredients=ingredients)
-    
+    if request.args.get("idDrink"):
+        idDrink = request.args.get("idDrink")
+
+        drink = db.execute("SELECT * FROM cocktails WHERE idDrink = ?", idDrink)[0]
+
+    else:
+
+        drink = db.execute("SELECT * FROM cocktails")[0]
+
+    ingredients = []
+
+    for n in range(drink["numIngredients"]):
+
+        item = {
+            "ingredient": drink[f"strIngredient{n+1}"],
+            "measure": drink[f"strMeasure{n+1}"],
+        }
+
+        ingredients.append(item)
+
+    return render_template(
+        "cocktails.html", cocktails=cocktails, drink=drink, ingredients=ingredients
+    )
 
 
+@app.route("/cabinet", methods=["GET", "POST"])
+def cabinet():
 
-"""
-@app.route("/search")
-def search():
-  cocktails = db.execute("SELECT * FROM cocktails WHERE strDrink LIKE ? ORDER BY strDrink", "%" + request.args.get("q") + "%")
-  return render_template("search.html", cocktails=cocktails)
-"""
+    if request.method == "POST":
 
-#@app.route("/retrieve",methods=["POST"])
-#def retrieve():
-#  id = request.form.get()
-  
+        haves = request.form.getlist("check")
+
+        db.execute("UPDATE ingredients SET have = 0")
+
+        for have in haves:
+            db.execute("UPDATE ingredients SET have = 1 WHERE item = ?", have)
+
+    items = db.execute(
+        "SELECT DISTINCT item, category, have FROM ingredients ORDER BY item"
+    )
+
+    entries = []
+    # declare categories set to obtain unique values
+    categories = []
+
+    for item in items:
+
+        entry = item["item"]
+        category = item["category"]
+
+        if item["have"]:
+            status = "checked"
+        else:
+            status = "unchecked"
+
+        entries.append({"item": entry, "category": category, "status": status})
+
+        # add category to set
+        categories.append(category)
+
+    # convert categories set back to an ordered list
+    categories = set(categories)
+    list(categories).sort()
+
+    return render_template("cabinet.html", entries=entries, categories=categories)
